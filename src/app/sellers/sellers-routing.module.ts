81b9314acaf3885eaf3dc5787f0da69e38820882
Miguel Peña <miguel9019@gmail.com>
2018-08-13 15:48:05 -0400

    Trabajndo en sellers y leads. Ya sin errores de auth

--- src/app/sellers/sellers-routing.module.ts
+++ src/app/sellers/sellers-routing.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SellersComponent } from './sellers/sellers.component';
 import { AuthGuardService } from '../core/services/guards/auth-guard.service';
+import {SellerSidenavComponent} from './components/seller-sidenav/seller-sidenav.component';
 
 const routes: Routes = [
   {
     path: '',
     component: SellersComponent,
     canActivate: [AuthGuardService]
+  },
+  {
+    path: 'seller/:id',
+    component: SellerSidenavComponent
   }
 ];
 
